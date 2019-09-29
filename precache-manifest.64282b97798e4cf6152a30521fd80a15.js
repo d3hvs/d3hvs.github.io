e67bcf927ae1686aed01d840aac04cda7f332b09
David Higuita <david.higuita.75@.com>
2019-09-29 17:54:00 -0500

    Ran build script

---
+++ precache-manifest.64282b97798e4cf6152a30521fd80a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ae4d7d07900e6ce804f0",
+    "url": "/static/js/main.ae4d7d07.chunk.js"
+  },
+  {
+    "revision": "98dbccfe9661e6fee0fc",
+    "url": "/static/js/2.98dbccfe.chunk.js"
+  },
+  {
+    "revision": "ae4d7d07900e6ce804f0",
+    "url": "/static/css/main.db42d356.chunk.css"
+  },
+  {
+    "revision": "98dbccfe9661e6fee0fc",
+    "url": "/static/css/2.6dbc4265.chunk.css"
+  },
+  {
+    "revision": "112a286de965340f2946ac22cb532406",
+    "url": "/index.html"
+  }
+];
